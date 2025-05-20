6c52ed4a7ffc1d23c6aeb72473083f635de9cdb2
Shiv Satyam <84907785+demss233@users.noreply.github.com>
2025-05-20 22:44:30 +0530

    Update Links.tsx

--- components/Links.tsx
+++ components/Links.tsx
@@ -5,7 +5,7 @@ export default function LinkHeader({ headers }) {
     <h1
       className="text-1xl bg-[#232323] p-2 text-white sm:w-[100%]  sm:h-[90px] lg:h-[60px] items-center flex rounded-tl-2xl rounded-br-2xl hover:rounded-tl-[0px] transition-all hover:rounded-br-[0px] cursor-pointer"
       onClick={() => {
-        window.location.href = `http://localhost:3000/chapter/${headers[0]}`;
+        window.location.href = `https://bhagavad-eight.vercel.app/chapter/${headers[0]}`;
       }}
     >{`${headers[0]}. ${headers[1]} / ${headers[3]}`}</h1>
   );
